a620d70e4196ca0f5ed488c68de1a2df3e000e58
Ikechi <consumerike@gmail.com>
2017-03-28 18:55:07 -0500

    initialized todo state

--- src/app/index.js
+++ src/app/index.js
@@ -3,14 +3,25 @@ import ReactDOM from 'react-dom';
 
 //creating the first component
 class TodoComponent extends React.component {
+    constructor(props) {
+        super(props);
+        this.state = {todos: ['wash', 'eat cheese', 'nap']}
+    }
     render() {
         return (
-            <h1>Hey World!</h1>
+            <div>
+                <h1>Hey World!</h1>
+                <ul>
+                    <li>this.state.todos[0]</li>
+                    <li>this.state.todos[1]</li>
+                    <li>this.state.todos[2]</li>
+                </ul>
+            </div>
         );
     }
 }
 
-To insert component into HTML:
+// To insert component into HTML:
 ReactDom.render(<TodoComponent />, document.getElementById("todo-wrapper"));
 
 
