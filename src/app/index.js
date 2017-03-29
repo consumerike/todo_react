02202614e3a95d243e6b081366e9b07352bcb593
Ikechi <consumerike@gmail.com>
2017-03-29 14:52:04 -0500

    modularizing code off index.js

--- src/app/index.js
+++ src/app/index.js
@@ -37,34 +37,10 @@ class TodoComponent extends React.Component {
     }
 }
 
-class TodoItem extends React.Component {
-    render () {    
-        return(
-            <li>
-                <div className="todo-item">
-                    <span className="item-name">{this.props.item}</span>
-                    <span className="item-delete" onClick={this.handleDelete}>x</span>
-                </div>
-            </li>
-        );
-    }
-
-}
-
     handleDelete() {
         this.props.onDelete(this.props.item);
     }
 
-/* OR
-Create a todo item react-way
- let todoItem = React.createClass({
-    render: function(){
-         return (
-
-        );
-    }
- });
-*/
 
 // To insert component into HTML:
 ReactDOM.render(<TodoComponent />, document.getElementById("todo-wrapper"));
