e9883e4d62bb0a157399852810657d1b50d6d980
Ikechi <consumerike@gmail.com>
2017-03-29 14:53:22 -0500

    added todoitem module

---
+++ src/app/todoItem.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+//CSS requires
+require('./css/todoItem.css');
+
+class TodoItem extends React.Component {
+    render () {    
+        return(
+            <li>
+                <div className="todo-item">
+                    <span className="item-name">{this.props.item}</span>
+                    <span className="item-delete" onClick={this.handleDelete}>x</span>
+                </div>
+            </li>
+        );
+    }
+
+}
+
+module.exports = TodoItem;
+
+//Create TodoItem component the react way
+/*var TodoItem = React.createClass({
+    render: function(){
+        return(
+            <li>
+                <div className="todo-item">
+                    <span className="item-name" ref="thisItem">{this.props.item}</span>
+                    <span className="item-remove" onClick={this.handleDelete}> x </span>
+                </div>
+            </li>
+        );
+    },
+
+    //Custom functions
+    handleDelete: function(){
+        this.props.onDelete(this.props.item);
+    }
+});
+*/
+
